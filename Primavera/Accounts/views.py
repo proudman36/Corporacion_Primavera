46f4edc6fd947a461ee264cca38defefe0827a68
Lina Caceres Osma <116388138+linacaceres17@users.noreply.github.com>
2023-03-08 11:59:59 -0500

    Update views.py

--- Primavera/Accounts/views.py
+++ Primavera/Accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import authenticate, login, logout
 
@@ -7,6 +7,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
+            return redirect('Accounts:login')
     else:
         form = RegisterForm()
     
