93bdb857f2350a8eec11eac77f75f051d61dfad5
Juan Esteban  Morales Quevedo <proudman36@MacBook-Pro-de-Juan.local>
2023-03-08 14:51:07 -0500

    Database

--- Primavera/news/urls.py
+++ Primavera/news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
 
+app_name = 'news'
 urlpatterns = [
     path('news/', views.index,name='index'),
     path('post_news/',views.post_news,name="post_news")
