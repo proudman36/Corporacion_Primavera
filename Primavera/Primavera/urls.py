80da8976d89318f0db998045807b654d4966eaf4
Juan Esteban  Morales Quevedo <proudman36@MacBook-Pro-de-Juan.local>
2023-03-08 10:59:36 -0500

    Part of the news

--- Primavera/Primavera/urls.py
+++ Primavera/Primavera/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -22,4 +24,6 @@ urlpatterns = [
     path('',include('home.urls')),
     path('',include('Accounts.urls')),
     path('',include('django.contrib.auth.urls')),
+    path('',include('news.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
