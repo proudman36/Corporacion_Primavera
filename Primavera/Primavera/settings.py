bbaef1ea7687f7b5bcd495ed4c6a0fb0a0c6f210
linacaceres17 <linchan17@gmail.com>
2023-03-09 16:33:47 -0500

    add css to the home

--- Primavera/Primavera/settings.py
+++ Primavera/Primavera/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIR =(os.path.join(BASE_DIR, 'about/static/'),) 
+STATICFILES_DIR =(os.path.join(BASE_DIR, 'home/static/'),) 
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
