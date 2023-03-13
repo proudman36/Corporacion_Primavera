27b889e9f38fec771f41881279c2097048e8e627
Juan Esteban  Morales Quevedo <proudman36@MacBook-Pro-de-Juan.local>
2023-03-13 15:35:36 -0500

    First attempt

--- Primavera/forms/views.py
+++ Primavera/forms/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render,redirect
-from django.core.mail import EmailMessage
+from django.shortcuts import render
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.shortcuts import redirect
 from django.conf import settings
 
-def index(response):
-    return render(response,'form/index.html',{})
+def index(request):
+    return render(request, 'form/index.html')
 
 def contact(request):
     if request.method == 'POST':
@@ -16,13 +17,8 @@ def contact(request):
         message = request.POST['message']
         from_email = settings.EMAIL_HOST_USER
         recipient_list = ['newhamonesjames@gmail.com']
-        email = EmailMessage(
-                'Mensaje de app Django',
-                'El usuario con nombre {} con la dirección {} escribe lo siguiente:\n\n {}'.format(name,email,message),
-                '',
-                ['newhamonesjames@gmail.com'],
-                reply_to=email
-
-                
-                  )
-        email.send()
+        email_body = render_to_string('form/email_body.html', {'name': name, 'email': email, 'subject': subject, 'phone': phone, 'country': country, 'message': message})
+        send_mail(subject, email_body, from_email, recipient_list, fail_silently=False, html_message=email_body)
+        return redirect('index')
+    else:
+        return render(request, 'form/index.html', {})
